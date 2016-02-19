d66541a28635b8d3217b804deeb007d501591ae2
Erin Swenson-Healey <eswenson-healey@cj.com>
2016-02-19 10:31:21 -0800

    Log requests to stdout

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Web.Scotty
-import Data.Monoid (mconcat)
+import           Data.Monoid                          (mconcat)
+import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import           Web.Scotty
 
 main = scotty 3000 $ do
+  middleware logStdoutDev
   get "/:word" $ do
     beam <- param "word"
     html $ mconcat [ "<h1>Scotty, "
