131dc74987608b0fce68c74c3b0b1e4c250eb70c
Erin Swenson-Healey <eswenson-healey@cj.com>
2016-02-19 16:36:32 -0800

    Use a less-dumb logger

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import           Data.Monoid                          (mconcat)
-import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import           Network.Wai.Middleware.RequestLogger (logStdout)
 import           Web.Scotty
 
 main = scotty 8080 $ do
-  middleware logStdoutDev
+  middleware logStdout
   get "/:word" $ do
     beam <- param "word"
     html $ mconcat [ "<h1>Scotty, "
